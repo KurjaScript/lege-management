43db48ded6c147af91403ed63271e019cfdabd11
Kurja <kurja0403@gmail.com>
2022-12-27 22:30:38 +0800

    按需引入antd

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import { useState } from 'react'
+import { Button } from 'antd'
+// import 'antd/dist/reset.css'
+import { UpCircleOutlined } from '@ant-design/icons'
 
 function App() {
   const [count, setCount] = useState(0)
 
-  return <div className="App">顶级组件</div>
+  return (
+    <div className="App">
+      顶级组件
+      <Button type="primary">我们的按钮</Button>
+      <UpCircleOutlined
+        style={{ fontSize: '40px', color: 'red' }}></UpCircleOutlined>
+    </div>
+  )
 }
 
 export default App
