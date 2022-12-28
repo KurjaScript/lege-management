729d1073fc9fb518271246cc97f7d7400a2dd18b
Kurja <kurja0403@gmail.com>
2022-12-28 10:57:56 +0800

    用户访问'/'路径，重定向到'/home'路径

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,7 +1,7 @@
 import App from '../App'
 import Home from '../views/Home'
 import About from '../views/About'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 // 两种路由模式的组件： BrowserRouter ( History 模式 )，HashRouter ( Hash 模式 )
 
 // const baseRouter = () => {
@@ -21,6 +21,8 @@ const baseRouter = () => (
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />}>
+        {/* 用户访问'/'路径，重定向到'/home'路径 */}
+        <Route path="/" element={<Navigate to="/home" />}></Route>
         <Route path="/home" element={<Home />}></Route>
         <Route path="/about" element={<About />}></Route>
       </Route>
