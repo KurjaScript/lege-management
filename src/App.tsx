29ce3776421dedbbe9bcdd3485397edac0cd9507
Kurja <kurja0403@gmail.com>
2022-12-28 10:48:42 +0800

    实现路由跳转

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
 import { useState } from 'react'
-import { Button } from 'antd'
+// import { Button } from 'antd'
 // import 'antd/dist/reset.css'
-import { UpCircleOutlined } from '@ant-design/icons'
-import { Outlet } from 'react-router-dom'
+// import { UpCircleOutlined } from '@ant-design/icons'
+import { Outlet, Link } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      {/* 顶级组件
-      <Button type="primary">我们的按钮</Button>
-      <UpCircleOutlined
-        style={{ fontSize: '40px', color: 'red' }}></UpCircleOutlined> */}
+      <Link to="/home">Home</Link>
+      <Link to="/about">About</Link>
       {/* 站位符组件，类似于窗口，用来展示组件，有点像 vue 里的 router-view */}
       <Outlet></Outlet>
     </div>
