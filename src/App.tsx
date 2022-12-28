7a7a6caea2f7acf74889ff55d83125dca4f7c817
Kurja <kurja0403@gmail.com>
2022-12-28 11:51:01 +0800

    路由懒加载

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ function App() {
     <div className="App">
       <Link to="/home">Home</Link>
       <Link to="/about">About</Link>
+      <Link to="/user">User</Link>
       {/* 站位符组件，类似于窗口，用来展示组件，有点像 vue 里的 router-view */}
       {/* <Outlet></Outlet> */}
       {outlet}
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,5 +1,10 @@
+import React, { lazy } from 'react'
 import Home from '../views/Home'
-import About from '../views/About'
+// import About from '../views/About'
+// import User from '../views/User'
+const About = lazy(() => import('../views/About'))
+const User = lazy(() => import('../views/User'))
+// 懒加载模式的组件的写法，外面需要套一层 Loading 的提示加载组件
 // 重定向组件
 import { Navigate } from 'react-router-dom'
 
@@ -14,7 +19,19 @@ const routes = [
   },
   {
     path: '/about',
-    element: <About />,
+    element: (
+      <React.Suspense fallback={<div>Loading</div>}>
+        <About />
+      </React.Suspense>
+    ),
+  },
+  {
+    path: '/user',
+    element: (
+      <React.Suspense fallback={<div>Loading</div>}>
+        <User />
+      </React.Suspense>
+    ),
   },
 ]
 
