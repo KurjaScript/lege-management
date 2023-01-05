5d2f173a8f3db0d45fb5ba6488a72b681e6c5330
Kurja <kurja0403@gmail.com>
2023-01-05 23:43:48 +0800

    菜单栏设置只有一个展开项

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -31,7 +31,7 @@ function getItem(
 const items: MenuItem[] = [
   getItem('Option 1', '/page1', <PieChartOutlined />),
   getItem('Option 2', '/page2', <DesktopOutlined />),
-  getItem('User', '/sub1', <UserOutlined />, [
+  getItem('User', 'sub1', <UserOutlined />, [
     getItem('Tom', '3'),
     getItem('Bill', '4'),
     getItem('Alex', '5'),
@@ -47,10 +47,17 @@ const View: React.FC = () => {
   const [collapsed, setCollapsed] = useState(false)
   const navigateTo = useNavigate()
 
-  const menuClick = (e) => {
+  const menuClick = (e: { key: string }) => {
     console.log('点击了菜单', e.key)
     // 点击跳转到对应的路由  编程式导航跳转
     navigateTo(e.key)
+  }
+  const [openKeys, setOpenKeys] = useState([''])
+  const handleOpenChange = (keys: string[]) => {
+    // 展开或回收某个菜单栏时执行这里的代码
+    console.log(keys) // keys 是一个数组，记录当前展开的那一项（用 key 记录）
+    // 把数组修改为最后一项，因为只要一项是展开的，就是刚刚点击的那一项
+    setOpenKeys([keys.pop()])
   }
 
   return (
@@ -63,10 +70,14 @@ const View: React.FC = () => {
         <div className="logo" />
         <Menu
           theme="dark"
-          defaultSelectedKeys={['1']}
+          defaultSelectedKeys={['/page1']}
           mode="inline"
           items={items}
           onClick={menuClick}
+          // 当前菜单展开和回收事件
+          onOpenChange={handleOpenChange}
+          // 当前菜单展开项的 key 数组
+          openKeys={openKeys}
         />
       </Sider>
       {/* 右边内容 */}
